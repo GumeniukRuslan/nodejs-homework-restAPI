0279245a3b8ce152c5cc93a76d3d82a577bb73c0
GumeniukRuslan <bishob1997@gmail.com>
2023-09-04 18:54:53 +0300

    read, find by id, delete

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,20 @@
 const express = require('express')
+const contactsFunctions = require('../../models/contacts')
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = await contactsFunctions.listContacts()
+  res.status(200).send(contacts);
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+  const contact = await contactsFunctions.getContactById(contactId)
+  if (!contact) {
+    return res.status(404).json({ message: 'Not found' })
+  }
+  res.status(200).send(contact)
 })
 
 router.post('/', async (req, res, next) => {
@@ -15,7 +22,12 @@ router.post('/', async (req, res, next) => {
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+  const contact = await contactsFunctions.removeContact(contactId)
+  if (!contact) {
+    return res.status(404).json({ message: 'Not found' })
+  }
+  res.status(200).json({ message: 'contact deleted' })
 })
 
 router.put('/:contactId', async (req, res, next) => {
